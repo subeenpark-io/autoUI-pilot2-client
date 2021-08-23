e67d361433c05081e49aeeff623353c4e4e6d3ef
Subeen Park <psb021414@gmail.com>
2021-08-24 05:03:16 +0900

    feat: routing

---
+++ svelte-app/src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import Home from "../components/Home.svelte";
+  import About from "../components/About.svelte";
+  import Blog from "../components/Blog.svelte";
+  import Before from "../components/Before.svelte";
+  import After from "../components/After.svelte";
+  import PostHoc from "../components/PostHoc.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="blog" component={Blog} />
+    <Route path="about" component={About} />
+    <Route path="/" component={Home} />
+    <Route path="before" component={Before} />
+    <Route path="after" component={After} />
+    <Route path="posthoc" component={PostHoc} />
+  </div>
+</Router>
